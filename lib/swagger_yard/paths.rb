7fc4498e830b5fb108bb52b5e4ef5edbd65425cf
Justin Madsen <jmadsen@simplenexus.com>
2023-04-18 12:21:09 -0600

    Raise error when duplicate path operation exists

--- lib/swagger_yard/paths.rb
+++ lib/swagger_yard/paths.rb
@@ -13,7 +13,24 @@ module SwaggerYard
     def merge(other)
       merged_items = {}
       (paths + other.paths).uniq.each do |path|
-        merged_items[path] = (path_items[path] || PathItem.new) + (other.path_items[path] || PathItem.new)
+        path_item = path_items[path] || PathItem.new
+        other_path_item = other.path_items[path] || PathItem.new
+        duplicate_operations = path_item.operations.keys & other_path_item.operations.keys
+        if duplicate_operations.present?
+          operations_info = duplicate_operations.map do |operation_key|
+            resource = path_item.operations[operation_key].extended_attributes["x-api-resource"]
+            other_resource = other_path_item.operations[operation_key].extended_attributes["x-api-resource"]
+            resources_info = [resource, other_resource].compact.map do |r|
+              "#{r["class"]}.#{r["method"]}"
+            end
+            info = "#{operation_key}"
+            info += " (#{resources_info.join(", ")})" if resources_info.present?
+            info
+          end
+          raise "Found duplicate operations for the same path (path: '#{path}', operations: #{operations_info.join(", ")})"
+        end
+
+        merged_items[path] = path_item + other_path_item
       end
       Paths.new(merged_items)
     end
